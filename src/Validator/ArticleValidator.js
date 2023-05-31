3862ecc9f2cac94b2145e33aad84e8e86202b15f
Shashi-Xcool <shashi.lata@xcool.in>
2023-05-31 18:33:34 +0530

    updated

--- src/Validator/ArticleValidator.js
+++ src/Validator/ArticleValidator.js
@@ -6,8 +6,8 @@ class articleValidator {
   static article() {
     return [
       param("userId").exists(),
-      body("title", "title is required").isAlphanumeric(),
-      body("description", "description is required").isAlphanumeric(),
+      body("title", "title is required").isString(),
+      body("description", "description is required").isString(),
     ];
   }
  
